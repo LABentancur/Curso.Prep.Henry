f7c0e57db4d435eaf2f7bd3c3a4a89363da65f9f
LABentancur <86126799+LABentancur@users.noreply.github.com>
2021-06-20 18:50:38 -0300

    Update homework.js
    La verdad que intento hacer los ejercicios pero no tengo la mas palida idea de como subirlos cuando los quiero subir no lo hace ya me canso

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "comercio";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 1250;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
@@ -34,6 +34,7 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+    Suma=x+y
 }
 
 function resta(x, y) {
